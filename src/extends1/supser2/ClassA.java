be21137ef43d2e7d9b4b47941fb3344a10b8c773
Hyeona You <youhyeona99@gmail.com>
2025-06-19 09:22:44 +0900

    상속

---
+++ src/extends1/supser2/ClassA.java
@@ -0,0 +1,8 @@
+package extends1.supser2;
+
+public class ClassA {
+
+    public ClassA() {
+        System.out.println("ClassA 생성자");
+    }
+}
---
+++ src/extends1/supser2/ClassC.java
@@ -0,0 +1,9 @@
+package extends1.supser2;
+
+public class ClassC extends ClassB {
+
+    public ClassC() {
+        super(10,20);
+        System.out.println("ClassC 생성자");
+    }
+}
