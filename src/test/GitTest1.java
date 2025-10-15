d0a96cf24fea2741251a210e5491cf57a67e6a44
Hyeona You <youhyeona99@gmail.com>
2025-10-15 18:55:10 +0900

    첫번째 커밋

---
+++ src/test/GitTest1.java
@@ -0,0 +1,11 @@
+package test;
+
+public class GitTest1 {
+    public static void main(String[] args) {
+
+        int a = 1;
+        int b = 2;
+
+        System.out.println("첫번째 커밋 1 ");
+    }
+}
